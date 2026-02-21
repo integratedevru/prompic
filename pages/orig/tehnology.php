38695803d5bf57264fe923af9b2ab5212c75e112
Jura Shekhanov <morovinger88@gmail.com>
2026-02-21 22:17:46 +0300

    Refactor homepage slider implementation and update CSS styles
    - Replaced hardcoded image elements in the homepage slider with a dynamic PHP loop to improve maintainability and scalability.
    - Updated CSS styles for the new slider structure, ensuring consistent image sizing across different screen resolutions.
    - Removed outdated CSS classes related to the previous slider implementation to declutter the stylesheet.

--- pages/orig/tehnology.php
+++ pages/orig/tehnology.php
@@ -50,63 +50,18 @@
 </div>
             </section>
 
+            <?php
+                $galleries = require __DIR__ . '/../../config/gallery.php';
+                $productionImages = $galleries[0]['images'];
+            ?>
             <section class="homepage_slider_2">
                 <div class="homepage_slider_2_container">
                     <div class="sobstvennoe_proizvodstvo_slider">
-<div>
-                            <img src="../img/homepage/slider2/slider2_foto1.webp" alt="Slide 1" class="slider_2_foto_1">
-                        </div>
+                        <?php foreach ($productionImages as $i => $image): ?>
                         <div>
-                            <img src="../img/homepage/slider2/slider2_foto2.webp" alt="Slide 2" class="slider_2_foto_2">
+                            <img src="<?= htmlspecialchars($image['src']) ?>" alt="Slide <?= $i + 1 ?>">
                         </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto3.webp" alt="Slide 3" class="slider_2_foto_3">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto4.webp" alt="Slide 4" class="slider_2_foto_4">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto5.webp" alt="Slide 5" class="slider_2_foto_5">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto6.webp" alt="Slide 6" class="slider_2_foto_6">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto7.webp" alt="Slide 7" class="slider_2_foto_7">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto8.webp" alt="Slide 8" class="slider_2_foto_8">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto9.webp" alt="Slide 9" class="slider_2_foto_9">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto10.webp" alt="Slide 10" class="slider_2_foto_10">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto11.webp" alt="Slide 11" class="slider_2_foto_11">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto12.webp" alt="Slide 12" class="slider_2_foto_12">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto13.webp" alt="Slide 13" class="slider_2_foto_13">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto14.webp" alt="Slide 14" class="slider_2_foto_14">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto15.webp" alt="Slide 15" class="slider_2_foto_14">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto16.webp" alt="Slide 16" class="slider_2_foto_14">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto17.webp" alt="Slide 17" class="slider_2_foto_14">
-                        </div>
-                        <div>
-                            <img src="../img/homepage/slider2/slider2_foto18.webp" alt="Slide 18" class="slider_2_foto_14">
-                        </div>
+                        <?php endforeach; ?>
                     </div>
                     <button type="button" class="custom-prev">
                         <img src="../img/homepage/slider2/button_custom_prev.svg" alt="Previous">
